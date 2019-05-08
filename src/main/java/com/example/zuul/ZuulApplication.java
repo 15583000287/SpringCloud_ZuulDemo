ea9ce83d7806e4362aa2bdf4ec87d7f216acc60c
wangxu <593464677@qq.com>
2019-05-09 00:48:55 +0800

    网关配置放在SpringCloudConfig配置中心

--- src/main/java/com/example/zuul/ZuulApplication.java
+++ src/main/java/com/example/zuul/ZuulApplication.java
@@ -1,17 +1,37 @@
 package com.example.zuul;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @EnableDiscoveryClient
-@EnableZuulProxy
+@EnableZuulProxy //开启zuul网关代理
 @SpringBootApplication
+@RefreshScope  // 使用该注解的类，会在接到SpringCloud配置中心配置刷新的时候自动刷新
+@RestController
 public class ZuulApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ZuulApplication.class, args);
     }
 
+    //zuul配置能够使用config实现实时跟新
+//    @ConfigurationProperties("zuul")
+//    public ZuulProperties zuulProperties(){
+//        return new ZuulProperties();
+//    }
+    @Value("${zuul.routes.api-a.path}")
+    String path;
+    @GetMapping("/getconfig")
+    public String test(){
+        System.out.println("spring.datasource.type ="+path);
+        return path;
+    }
 }
